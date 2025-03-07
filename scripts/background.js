fd5d6e98ca10c113978152bd5cc4cd5114fb43bd
V-Srivatsan <vatsan.programming@gmail.com>
2025-03-07 19:23:24 +0530

    Added test for website url

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,11 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     // Forward user prompt to content script
     chrome.tabs.query({ active: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, message, (res) => sendResponse(res));
+        const re = new RegExp("https://.*\.vit.ac.in/.*")
+        tabs.forEach(tab => {
+            if (re.test(tab.url))
+                chrome.tabs.sendMessage(tabs[0].id, message, (res) => sendResponse(res));
+        })
     });
     return true;
 });
